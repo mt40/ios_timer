556f317c6b56c958ca070b6897804c7e3b9827db
Minh Thai <minhthai40@gmail.com>
2021-02-20 10:20:32 +0700

    Initial Commit

---
+++ ios_timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_timer
+//
+//  Created by Thai Minh on 20/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
